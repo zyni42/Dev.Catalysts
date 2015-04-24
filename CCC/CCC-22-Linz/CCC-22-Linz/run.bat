3cbc8568fd312a3c87865bde83fa0bb90dba6c1a
Harakieri <harakieri@ymail.com>
2015-04-24 14:18:16 +0200

    l1

--- CCC/CCC-22-Linz/CCC-22-Linz/run.bat
+++ CCC/CCC-22-Linz/CCC-22-Linz/run.bat
@@ -7,26 +7,23 @@ IF "%1" == ""  SET USAGE=YES
 
 IF "%USAGE%" == "YES" (
 	ECHO :
-	ECHO : USAGE: %0  LEVEL [ SPEED ]
+	ECHO : USAGE: %0
 	ECHO :
 	ECHO :            LEVEL = 1, 2, 3, 4, 5, 6, 7
-	ECHO :            SPEED = 1, 2, 3, ... 100
 	ECHO :
-	ECHO : EXAMPLE : %0 6 10 ..... run the 6th level at speed 10
+	ECHO : EXAMPLE : %0 6 ..... run the 6th level
 	ECHO :
 	GOTO END
 )
 
 set LEVEL=level6.in
-set SPEED=1
 
 IF NOT "%1" == "" set LEVEL=level%1.in
-IF NOT "%2" == "" set SPEED=%2
 
 REM cd bin\debug
 
-ECHO java  -jar simulator.jar --exec="bin\debug\CCC-22-Linz.exe %1" --level=Level%1\%LEVEL% --speed=%SPEED%
-     java  -jar simulator.jar --exec="bin\debug\CCC-22-Linz.exe %1" --level=Level%1\%LEVEL% --speed=%SPEED%
+ECHO java  -jar simulator.jar --exec="bin\debug\CCC-22-Linz.exe %1" --level=Level%1\%LEVEL%
+     java  -jar simulator.jar --exec="bin\debug\CCC-22-Linz.exe %1" --level=Level%1\%LEVEL%
 
 REM cd ..\..
 
