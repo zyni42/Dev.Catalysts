685411d3150b4abc01b991dfa308842f3199ac22
Harakieri <harakieri@ymail.com>
2015-11-06 13:32:41 +0100

    run.bat corr

--- CCC/CCC-23-Rathaus/CCC-23-Rathaus/run.bat
+++ CCC/CCC-23-Rathaus/CCC-23-Rathaus/run.bat
@@ -24,8 +24,8 @@ IF NOT "%2" == "" set LEVEL=level%2\level%2.in
 
 REM cd bin\debug
 
-ECHO java  -jar simulator.jar --exec="bin\debug\CCC-23-Linz.exe %1 %3" --level=%LEVEL%
-     java  -jar simulator.jar --exec="bin\debug\CCC-23-Linz.exe %1 %3" --level=%LEVEL%
+ECHO java  -jar simulator.jar --exec="bin\Debug\CCC-23-Rathaus.exe %1 %3" --level=%LEVEL%
+     java  -jar simulator.jar --exec="bin\Debug\CCC-23-Rathaus.exe %1 %3" --level=%LEVEL%
 
 REM cd ..\..
 
