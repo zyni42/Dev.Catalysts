615a14d90a78fe17fe6c56df4165c2656bb55b2f
Harakieri <harakieri@ymail.com>
2015-11-08 14:55:02 +0100

    Keyboard Control

--- CCC/CCC-23-Rathaus/CCC-23-Rathaus/Program.cs
+++ CCC/CCC-23-Rathaus/CCC-23-Rathaus/Program.cs
@@ -13,12 +13,12 @@ namespace CCC_23_Rathaus
 			var vis = new Vis.CCC23RathausView ();
 			Class2 l2 = null;
 			//l2 = new Class2 ("..\\..\\Level2\\level2_example.in"); l2.CalculateResult ();
-			Car1.ResetCarNames (); l2 = new Class2 ("..\\..\\Level2\\level2_1.in"); l2.CalculateResult (vis);
-			Car1.ResetCarNames (); l2 = new Class2 ("..\\..\\Level2\\level2_2.in"); l2.CalculateResult (vis);
-			Car1.ResetCarNames (); l2 = new Class2 ("..\\..\\Level2\\level2_3.in"); l2.CalculateResult (vis);
-			Car1.ResetCarNames (); l2 = new Class2 ("..\\..\\Level2\\level2_4.in"); l2.CalculateResult (vis);
-			Car1.ResetCarNames (); l2 = new Class2 ("..\\..\\Level2\\level2_5.in"); l2.CalculateResult (vis);
-			Car1.ResetCarNames (); l2 = new Class2 ("..\\..\\Level2\\level2_6.in"); l2.CalculateResult (vis);
+			Car1.ResetCarNames (); l2 = new Class2 ("..\\..\\Level2\\level2_1.in"); if (l2.CalculateResult (vis)) return;
+			Car1.ResetCarNames (); l2 = new Class2 ("..\\..\\Level2\\level2_2.in"); if (l2.CalculateResult (vis)) return;
+			Car1.ResetCarNames (); l2 = new Class2 ("..\\..\\Level2\\level2_3.in"); if (l2.CalculateResult (vis)) return;
+			Car1.ResetCarNames (); l2 = new Class2 ("..\\..\\Level2\\level2_4.in"); if (l2.CalculateResult (vis)) return;
+			Car1.ResetCarNames (); l2 = new Class2 ("..\\..\\Level2\\level2_5.in"); if (l2.CalculateResult (vis)) return;
+			Car1.ResetCarNames (); l2 = new Class2 ("..\\..\\Level2\\level2_6.in"); if (l2.CalculateResult (vis)) return;
 		}
 	}
 }
