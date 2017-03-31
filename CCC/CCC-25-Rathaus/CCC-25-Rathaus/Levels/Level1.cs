c91289828377fa95afe6720b3ea236ef07c1e8ca
zyni42 <zyni42@hotmail.com>
2017-03-31 13:45:42 +0200

    Level1 hinzugefügt.

---
+++ CCC/CCC-25-Rathaus/CCC-25-Rathaus/Levels/Level1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CCC_25_Rathaus.Levels
+{
+	class Level1 : BaseStuff.ICccLevel<string[]>
+	{
+		public string[] CalculateResult()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
