c79c83dafa3c5dd560f6a89ec63204ce72c49eeb
Harakieri <harakieri@ymail.com>
2017-04-01 03:25:48 +0200

    Level 6 - coded & tested ... but (for the moment) no way of UPLOADING results (for verification)

--- CCC/CCC-25-Rathaus/CCC-25-Rathaus/Levels/Level6.cs
+++ CCC/CCC-25-Rathaus/CCC-25-Rathaus/Levels/Level6.cs
@@ -16,88 +16,110 @@ namespace CCC_25_Rathaus.Levels
 			long maxHyperDistance;
 			Dictionary<string, Station> stations;
 
-			//stations = ParseInput4(@"..\..\Levels\level4\level4-1.txt", out journeys, out numFastestHypers);
-			//resultList.Add(CalculateDuration4(stations, journeys, numFastestHypers).ToString());
+			stations = ParseInput6(@"..\..\Levels\level6\level6-1.txt", out journeys, out numFastestHypers, out maxHyperDistance);
+			resultList.Add ("6-1 ... " + CalculateDuration6 (stations, journeys, numFastestHypers, maxHyperDistance).ToString());
 
-			//stations = ParseInput4(@"..\..\Levels\level4\level4-2.txt", out journeys, out numFastestHypers);
-			//resultList.Add(CalculateDuration4(stations, journeys, numFastestHypers).ToString());
+			stations = ParseInput6 (@"..\..\Levels\level6\level6-2.txt", out journeys, out numFastestHypers, out maxHyperDistance);
+			resultList.Add ("6-2 ... " + CalculateDuration6 (stations, journeys, numFastestHypers, maxHyperDistance).ToString ());
 
-			//stations = ParseInput4(@"..\..\Levels\level4\level4-3.txt", out journeys, out numFastestHypers);
-			//resultList.Add(CalculateDuration4(stations, journeys, numFastestHypers).ToString());
+			stations = ParseInput6 (@"..\..\Levels\level6\level6-3.txt", out journeys, out numFastestHypers, out maxHyperDistance);
+			resultList.Add ("6-3 ... " + CalculateDuration6 (stations, journeys, numFastestHypers, maxHyperDistance).ToString ());
 
-			//stations = ParseInput4(@"..\..\Levels\level4\level4-4.txt", out journeys, out numFastestHypers);
-			//resultList.Add(CalculateDuration4(stations, journeys, numFastestHypers).ToString());
+			stations = ParseInput6 (@"..\..\Levels\level6\level6-4.txt", out journeys, out numFastestHypers, out maxHyperDistance);
+			resultList.Add ("6-4 ... " + CalculateDuration6 (stations, journeys, numFastestHypers, maxHyperDistance).ToString());
 
-			stations = ParseInput6(@"..\..\Levels\level6\level6-eg.txt", out journeys, out numFastestHypers, out maxHyperDistance);
-			//resultList.Add(CalculateDuration6(stations, journeys, numFastestHypers).ToString());
+			stations = ParseInput6 (@"..\..\Levels\level6\level6-eg.txt", out journeys, out numFastestHypers, out maxHyperDistance);
+			resultList.Add ("6-eg .. " + CalculateDuration6 (stations, journeys, numFastestHypers, maxHyperDistance).ToString ());
 
 			return resultList.ToArray ();
 		}
 
+		public class ParamsForGenerateNextHyperRoute
+		{
+			public Station [] HyperStations;
+			public Random     Random;
+			public long       MaxHyperDistance;
+			public long       MaxStations;
+		}
 
-		//Station [] GenerateNextHyperRoute (long maxHyperDistance, Dictionary<string, Station> stations, Journey[] journeys)
-		//{
-		//	var myHyperStationsTmp = new Dictionary<string, Station> ();
-		//	foreach (var singleJourney in journeys) {
-		//		if (!myHyperStationsTmp.ContainsKey (singleJourney.Start.Name))
-		//			myHyperStationsTmp.Add (singleJourney.Start.Name, singleJourney.Start);
-		//		if (!myHyperStationsTmp.ContainsKey (singleJourney.Stop.Name))
-		//			myHyperStationsTmp.Add (singleJourney.Stop.Name, singleJourney.Stop);
-		//	}
-		//	var myHyperStations = myHyperStationsTmp.Values.ToArray ();
-
-		//	Random c = new Random ();
-
-		//	var 
-
-		//	// random -> so lang
-		//	// for i = 0 ... so lang : random -> station ==INTO==> result
-		//	// if resullt ZU LANG re-loop
-
-		//	return result
-
-		//	var theIndexes = new int [myHyperStations.Length];
-		//	for ( int i = 0 ; i < theIndexes.Length; i++)
-		//		theIndexes[i] = i;
-
-			
-
-		//	var rc = new Station[myHyperStations.Length];
-		//	for ( int i = 0 ; i < myHyperStations.Length; i++) {
-		//		rc[i] = myHyperStations[ theIndexes[i] ];
-		//	}
-		//	return rc;
-		//}
+		Station[] GenerateNextHyperRoute (ParamsForGenerateNextHyperRoute paramValues)
+		{
+			const int ABS_MAX = 100;
+			for (;;) {
+				// random -> so lang
+				var numberOfStations = 2 + paramValues.Random.Next (Math.Min (paramValues.HyperStations.Length, ABS_MAX) - 1);
+				var hyperRouteArr = new Station[numberOfStations];
+				var usedSources = new bool[paramValues.HyperStations.Length];
+				double totalHyperDistance = 0;
+				// for i = 0 ... so lang : random -> station ==INTO==> result
+				for (int i = 0; i < numberOfStations; i++) {
+					int stationIdx = -1;
+					// avoid duplicate use of a station
+					while (stationIdx < 0 || usedSources[stationIdx])
+						stationIdx = paramValues.Random.Next (paramValues.HyperStations.Length);
+					usedSources[stationIdx] = true;
+					hyperRouteArr[i] = paramValues.HyperStations[stationIdx];
+					if (i > 0) {
+						totalHyperDistance += CalculateDistance (hyperRouteArr[i - 1], hyperRouteArr[i]);
+						// if result TOO LONG re-loop
+						if (totalHyperDistance > paramValues.MaxHyperDistance)
+							break;
+					}
+				}
+				// if result TOO LONG re-loop
+				if (totalHyperDistance > paramValues.MaxHyperDistance)
+					continue;
+				return hyperRouteArr;
+			}
+		}
 
 
-		//public string CalculateDuration6 (Dictionary<string, Station> stations, Journey[] journeys, long numFastestHypers, long maxHyperDistance)
-		//{
+		public string CalculateDuration6 (Dictionary<string, Station> stations, Journey[] journeys, long numFastestHypers, long maxHyperDistance)
+		{
+			DateTime starttime = DateTime.Now;
+			ParamsForGenerateNextHyperRoute paramValues = null;
+			{
+				var myHyperStationsTmp = new Dictionary<string, Station> ();
+				foreach (var singleJourney in journeys) {
+					if (!myHyperStationsTmp.ContainsKey (singleJourney.Start.Name)) myHyperStationsTmp.Add (singleJourney.Start.Name, singleJourney.Start);
+					if (!myHyperStationsTmp.ContainsKey (singleJourney.Stop .Name)) myHyperStationsTmp.Add (singleJourney.Stop .Name, singleJourney.Stop );
+				}
+				paramValues = new ParamsForGenerateNextHyperRoute () {
+					MaxHyperDistance = maxHyperDistance,
+					HyperStations = myHyperStationsTmp.Values.ToArray (),
+					MaxStations = 100,
+					Random = new Random ()
+				};
+			}
+			long numIterations = 0;
+			Station[] singleHyperRoute = null;
+			while ((singleHyperRoute = GenerateNextHyperRoute (paramValues)) != null) {
+				numIterations++;
+				long foundfasterJourneys = 0;
+				foreach (var singleJourney in journeys) {
+					var durationUsingHyper = CalculateDuration5 (
+						new Station[] { singleJourney.Start, singleJourney.Stop },
+						singleHyperRoute
+						);
+					if (durationUsingHyper < singleJourney.Duration)
+						foundfasterJourneys++;
+					if (foundfasterJourneys >= numFastestHypers) {
+						var rc = string.Format ("{0}", singleHyperRoute.Length);
+						foreach (var hop in singleHyperRoute) {
+							rc += string.Format (" {0}", hop.Name);
+						}
+						return
+							string.Format ("{0,-16} ({1,5} iterations)    -->    ", DateTime.Now - starttime, numIterations)
+							+
+							rc;
+					}
+				}
+			}
 
-		//	Station [] singleHyperRoute = null;
+			return "SEMMERL";
+		}
 
-		//	while ((singleHyperRoute = GenerateNextHyperRoute (maxHyperDistance, stations, journeys)) != null) {
-		//		long foundfasterJourneys = 0;
-		//		foreach (var singleJourney in journeys) {
-		//			var durationUsingHyper = CalculateDuration5 (
-		//				new Station[] { singleJourney.Start, singleJourney.Stop },
-		//				singleHyperRoute
-		//				);
-		//			if (durationUsingHyper < singleJourney.Duration)
-		//				foundfasterJourneys++;
-		//			if (foundfasterJourneys >= numFastestHypers) {
-		//				var rc = string.Format ("{0}", singleHyperRoute.Length);
-		//				foreach (var hop in singleHyperRoute) {
-		//					rc += string.Format (" {0}", hop.Name);
-		//				}
-		//				return rc;
-		//			}
-		//		}
-		//	}
 
-		//	return "SEMMERL";
-		//}
-
-		
 		public Dictionary<string, Station> ParseInput6 (string inputFile, out Journey[] journeys, out long numFastestHypers, out long maxHyperDistance)
 		{
 			Dictionary<string, Station> stations = new Dictionary<string, Station> ();
--- CCC/CCC-25-Rathaus/CCC-25-Rathaus/Program.cs
+++ CCC/CCC-25-Rathaus/CCC-25-Rathaus/Program.cs
@@ -13,23 +13,23 @@ namespace CCC_25_Rathaus
 		static void Main(string[] args)
 		{
 			Level1 lvl1 = new Level1();
-			var lvl1Result = lvl1.CalculateResult ();
+			//var lvl1Result = lvl1.CalculateResult ();
 			//var lvlResult = lvl1Result;
 
 			Level2 lvl2 = new Level2();
-			var lvl2Result = lvl2.CalculateResult ();
+			//var lvl2Result = lvl2.CalculateResult ();
 			//var lvlResult = lvl2Result;
 
 			Level3 lvl3 = new Level3();
-			var lvl3Result = lvl3.CalculateResult ();
+			//var lvl3Result = lvl3.CalculateResult ();
 			//var lvlResult = lvl3Result;
 
 			Level4 lvl4 = new Level4();
-			var lvl4Result = lvl4.CalculateResult ();
+			//var lvl4Result = lvl4.CalculateResult ();
 			//var lvlResult = lvl4Result;
 
 			Level5 lvl5 = new Level5();
-			var lvl5Result = lvl5.CalculateResult ();
+			//var lvl5Result = lvl5.CalculateResult ();
 			//var lvlResult = lvl5Result;
 
 			Level6 lvl6 = new Level6();
