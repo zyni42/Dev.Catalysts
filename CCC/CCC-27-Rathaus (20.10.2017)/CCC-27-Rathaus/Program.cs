5c51300303b897141e6571e5adad53908e50bf2a
zyni42 <zyni42@hotmail.com>
2017-10-19 23:34:27 +0200

    Added CCC-27-Rathaus project and renamed old contests (added date).

--- CCC/CCC-27-Rathaus (20.10.2017)/CCC-27-Rathaus/Program.cs
+++ CCC/CCC-27-Rathaus (20.10.2017)/CCC-27-Rathaus/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CCC_24_Linz
+namespace CCC_27_Rathaus
 {
 	class Program
 	{
--- CCC/CCC-27-Rathaus (20.10.2017)/CCC-27-Rathaus/Properties/AssemblyInfo.cs
+++ CCC/CCC-27-Rathaus (20.10.2017)/CCC-27-Rathaus/Properties/AssemblyInfo.cs
@@ -2,34 +2,34 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
+// General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("CCC-24-Linz")]
+[assembly: AssemblyTitle("20171020_CCC-27-Rathaus")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("CCC-24-Linz")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyProduct("20171020_CCC-27-Rathaus")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("8e724c2e-9b5c-4f42-93e2-674baaae197a")]
+[assembly: Guid("87c5c6ae-5c11-4cde-8126-b210bf19faf4")]
 
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
-//      Minor Version 
+//      Minor Version
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers 
+// You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
