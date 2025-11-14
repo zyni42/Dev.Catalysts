c80cdfc8febdba8d50b12b76a3d7d167149e9000
Valentin Eisler <leuchty2007@gmail.com>
2025-11-14 14:48:12 +0100

    Start

---
+++ CCC/CCC-41-Rathaus (14.11.2025)/CCC-41-Rathaus/Level.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CCC_41_Rathaus
+{
+    internal class Level
+    {
+        public static void Solve1(string[] rawInputData, StreamWriter sw)
+        {
+                
+        }
+    }
+}
--- CCC/CCC-41-Rathaus (14.11.2025)/CCC-41-Rathaus/Program.cs
+++ CCC/CCC-41-Rathaus (14.11.2025)/CCC-41-Rathaus/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection.Emit;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,8 +10,37 @@ namespace CCC_41_Rathaus
 {
 	internal class Program
 	{
-		static void Main(string[] args)
-		{
-		}
-	}
+
+        const string levelRootPathZyni = @"C:\Users\User42\Downloads\CCC-Levels\";
+        const string levelRootPathLeuchty = @"C:\Users\valen\source\repos\zyni42\Dev.Catalysts\CCC\CCC-41-Rathaus (14.11.2025)\Levels";
+        const int LEVEL = 1;
+        static string[] SUBLEVELS = { "small", "large" };
+        const bool EXAMPLE = true;
+        static void Main(string[] args)
+        {
+            if (EXAMPLE)
+            {
+                string[] rawInputData = File.ReadAllLines($"{levelRootPathLeuchty}\\level{LEVEL}\\level{LEVEL}_0_example.in");
+                StreamWriter outputFile = new StreamWriter($"{levelRootPathLeuchty}\\level{LEVEL}\\level{LEVEL}_0_my_example.out");
+                Level.Solve1(rawInputData, outputFile);
+                outputFile.Flush();
+                outputFile.Close();
+                return;
+            }
+            else
+            {
+                foreach (string subl in SUBLEVELS)
+                {
+                    string[] rawInputData = File.ReadAllLines($"{levelRootPathLeuchty}\\level{LEVEL}\\level{LEVEL}_{Array.IndexOf(SUBLEVELS, subl)}_{subl}.in");
+                    StreamWriter outputFile = new StreamWriter($"{levelRootPathLeuchty}\\level{LEVEL}\\level{LEVEL}_{Array.IndexOf(SUBLEVELS, subl)}_{subl}.out");
+                    Console.WriteLine(subl);
+                    outputFile.AutoFlush = true;
+                    Level.Solve1(rawInputData, outputFile);
+                    outputFile.Flush();
+                    outputFile.Close();
+                }
+
+            }
+        }
+    }
 }
