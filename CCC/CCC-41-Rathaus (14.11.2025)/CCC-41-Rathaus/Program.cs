1e66ae404f3517160c6eac63a3073c1c5126c6dd
zyni42 <zyni42@gmail.com>
2025-11-14 12:28:06 +0100

    Added CCC 41 Project

---
+++ CCC/CCC-41-Rathaus (14.11.2025)/CCC-41-Rathaus/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CCC_41_Rathaus
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+		}
+	}
+}
